d199ef9fccbc9069a530f07252f1bc6a62e00240
Bastian Doetsch <bdoetsch@vmware.com>
2021-05-07 16:45:54 +0200

    make sure mapped docker volume is absolute path

--- ci/startDb.sh
+++ ci/startDb.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
+set -ex
+BASH_LOADABLES_PATH=${BASH_LOADABLES_PATH:-/usr/local/lib/bash:/usr/lib/bash}
+enable -f realpath realpath
 
 dir="$(dirname "$0")"
 envFile=$1
+
+dbdata="$(realpath "$dir/../target/dbdata")"
+mkdir -p "$dbdata"
+
 # shellcheck disable=SC2091
 pushd "$dir" || $(echo "Can't enter directory $dir. Aborting." && exit 1)
-docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file "../$envFile" -v db-data:/database ibmcom/db2
+docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file "../$envFile" -v "$dbdata":/database ibmcom/db2
